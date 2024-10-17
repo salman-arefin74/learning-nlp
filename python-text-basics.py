1d5b66d430085fad95f0e5ed03ad3e70620455db
Salman Arefin <45092817+salman-arefin74@users.noreply.github.com>
2024-10-17 18:14:57 +0600

    Learning working with files in Python.

--- python-text-basics.py
+++ python-text-basics.py
@@ -33,6 +33,28 @@ Feynman    Physics                                95
 Hamilton   Mythology                             144
 """
 
+%%writefile test.txt
+Hello, this is a quick text file.
+This is the second line of the file.
+
+# The above command only works in Jupyter Notebook, this is for creating a text file.
+
+myfile = open('test.txt') # Open the text file
+myfile.read() # Read the opened file. This only works once because the pointer then points to EOF
+myfile.seek(0) # This resets the pointer
+
+content = myfile.read()
+print(content)
+
+myfile.close() # Close the file. You should always do it. It will cause issues if you open an already opened file from another program.
+
+mylines = myfile.readlines() # This reads every line as a seperate item.
+for line in mylines:
+  print(line)
+
+myfile = open('test.txt', 'w+') # To open in write mode. This will override the existing one.
+
+
 
 
 
